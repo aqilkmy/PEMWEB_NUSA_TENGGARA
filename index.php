0b9f08989e0770925f53ea54d5761fe6a2cfa407
Muhammad Aqil Karomy <131788757+aqilkmy@users.noreply.github.com>
2025-11-28 00:19:07 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nusa Tenggara Timur</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <div class="isi-navbar">
+            <div class="kiri-navbar">
+                <img src="asset/ntt.png" alt="" class="logo">
+                <p style="margin: 0px 20px;">Nusa Tenggara Timur</p>
+            </div>
+                <div class="kanan-navbar">
+                    <div class="nav-links">
+                        <a href="index.php">Beranda</a>
+                        <a href="#">Destinasi</a>
+                        <a href="#">Tentang</a>
+                        <a href="#">Kontak</a>
+                    </div>
+                    <div class="login-register">
+                        <a href="#" class="btn-login">Login</a>
+                        <a href="#" class="btn-register">Register</a>
+                    </div>
+                </div>
+        </div>
+    </nav>
+    <section class="hero">
+        <div class="hero-text">
+            <h1>Explore Nusa Tenggara Timur</h1>
+            <p>Discover the beauty and culture of Nusa Tenggara Timur, Indonesia's hidden gem.
+            From stunning beaches to vibrant traditions, embark on an unforgettable journey.
+            </p>
+            <a href="#" class="btn-hero">Get Started</a>
+
+        </div>
+        <div class="hero-img">
+            <img src="https://2.bp.blogspot.com/-3sr5Ru0WLrU/WixvQ0KYFwI/AAAAAAAAHwQ/G92was3cxtwTjyykjq1wE1A6p5UTuhwqwCLcBGAs/s1600/pulau%2Bpadar.jpg" alt="">
+        </div>
+    </section>
+</body>
+</html>
